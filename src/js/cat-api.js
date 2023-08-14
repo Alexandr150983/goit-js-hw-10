39a635ca0e74ff9e5b3a4a78eefcfd11901d7e6a
Alexandr150983 <alexandr150983@gmail.com>
2023-08-14 14:23:25 +0300

    Information about the cat

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -3,9 +3,8 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_fYN5fGg3mdb5Fw0OIRvriZCLS5fQpTjey6nNQ8dPQm3WCemqw2xtcmA1B7rfPXNK';
 
-export default function fetchBreeds() {
-  const baseUrl = 'https://api.thecatapi.com/';
-  const url = `${baseUrl}v1/breeds`;
+export function fetchBreeds() {
+  const url = `https://api.thecatapi.com/v1/breeds`;
 
   return axios.get(url).then(response => {
       if (response.status !== 200) {
@@ -13,4 +12,16 @@ export default function fetchBreeds() {
       }
       return response.data;
   });
+}
+
+export function fetchCatByBreed(breedId) {
+    const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+
+    return axios
+        .get(url)
+        .then(response => response.data)
+        .catch(error => {
+            console.log(error.message);
+            throw error;
+        });
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,32 @@
 import SlimSelect from 'slim-select';
 import Notiflix from 'notiflix';
 import axios from 'axios';
-import fetchBreeds from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_fYN5fGg3mdb5Fw0OIRvriZCLS5fQpTjey6nNQ8dPQm3WCemqw2xtcmA1B7rfPXNK';
 
 const breedSelect = document.querySelector('.breed-select');
+const catInfoDiv = document.querySelector('.cat-info');
   
+breedSelect.addEventListener('change', () => {
+    const selectedBreedId = breedSelect.value;
+
+    fetchCatByBreed(selectedBreedId)
+        .then(catData => {
+            catInfoDiv.innerHTML = `
+        <img src="${catData[0].url}" alt="Cat Image">
+        <h3>${catData[0].breeds[0].name}</h3>
+        <p><strong>Description:</strong> ${catData[0].breeds[0].description}</p>
+        <p><strong>Temperament:</strong> ${catData[0].breeds[0].temperament}</p>
+            `;
+            catInfoDiv.style.display = 'block';
+        })
+        .catch(error => {
+            console.log(error.message);
+            catInfoDiv.style.display = 'none';
+    })
+})
 fetchBreeds()
     .then(breeds => {
         breeds.forEach(breed => {
@@ -21,6 +40,10 @@ fetchBreeds()
     .catch(error => {
         console.log(error.message);
     });
+
+
+    
+
 
 new SlimSelect({
   select: '#selectElement',
